43919731e89f45da694be55ecc14df7f571453b4
jefferson08 <jeffersoncarvalho60@outlook.com>
2026-02-03 01:03:23 -0300

    chore: align drawer header padding

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -98,7 +98,7 @@ export function Header() {
               </Button>
             </SheetTrigger>
             <SheetContent side="right" className="w-72 p-0">
-              <SheetHeader className="flex flex-row items-center justify-between px-3 pt-3">
+              <SheetHeader className="flex flex-row items-center justify-between px-3 py-3">
                 <SheetTitle className="flex items-center gap-2 -ml-1">
                   <img
                     src="/icon.png"
