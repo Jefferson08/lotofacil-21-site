b0b85dd082cec60ce8707cd2076652f007430fc0
jefferson08 <jeffersoncarvalho60@outlook.com>
2026-02-03 02:28:16 -0300

    chore: configure gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { resolve } from "path"
 
 export default defineConfig({
-  base: "./",
+  base: "/lotofacil-21-site/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
